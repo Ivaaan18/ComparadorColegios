da0c8d93675aefd66665d0f42edd6a5afc3263fe
Ivan Aguilar Perez <ivanaguilarperez99@gmail.com>
2022-01-11 01:37:02 +0100

    Creacion del menú final

--- module1.py
+++ module1.py
@@ -10,3 +10,14 @@ from csv import reader
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+def bucle1():
+    respuesta =''
+    bandera = 0
+    print('Bienvenido al comparador de centros de la Comunidad de Madrid\n')
+    while respuesta != 'S' and respuesta != 'N':
+        respuesta = input('Si es la primera vez que ejecutas el programa diga S (S o N) ')
+        respuesta = respuesta.upper()
+    if respuesta != 'S' and respuesta != 'N':
+        print('¡¡ERROR!!: Escriba S o N')
+
+    return respuesta
