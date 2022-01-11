cb9660d6af6150a892f2b1567e091fd2ef2900d4
Ivan Aguilar Perez <ivanaguilarperez99@gmail.com>
2022-01-11 01:30:23 +0100

    Funciones del proyecto

---
+++ module1.py
@@ -0,0 +1,12 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+from bs4 import BeautifulSoup
+import os
+from os import remove
+import shutil
+from csv import reader
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
